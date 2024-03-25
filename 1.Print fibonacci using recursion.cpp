1aa06ced56b61edde8ca415f308e518586338ddc
Charitha Reddy <129163130+192210349@users.noreply.github.com>
2024-03-25 12:26:10 +0530

    Update 1.Print fibonacci using recursion.cpp

--- 1.Print fibonacci using recursion.cpp
+++ 1.Print fibonacci using recursion.cpp
@@ -1,33 +1,23 @@
 #include <stdio.h>
 
-int power(int base, int exponent) {
-    if (exponent == 0)
-        return 1;
-    return base * power(base, exponent - 1);
-}
-
-int countDigits(int num) {
-    if (num == 0)
-        return 0;
-    return 1 + countDigits(num / 10);
-}
-
-int isArmstrong(int num, int originalNum) {
-    if (num == 0)
-        return 0;
-    return power(num % 10, countDigits(originalNum)) + isArmstrong(num / 10, originalNum);
+// Function to calculate Fibonacci series using recursion
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n-1) + fibonacci(n-2);
 }
 
 int main() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
+    int n, i;
 
-    if (isArmstrong(num, num) == num)
-        printf("%d is an Armstrong number.\n", num);
-    else
-        printf("%d is not an Armstrong number.\n", num);
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
 
     return 0;
 }
-
